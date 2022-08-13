1b7588ea74e8f148522f6daeb90bb36e08651879
redrum <urecherrydream@gmail.com>
2022-08-13 19:39:45 +0300

    1st

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Technologies from './components/Technologies'
+import Header from './components/Header'
+import Raiting from './components/Raiting'
+import Accordion from './components/Accordion'
+import starlogo from './star.png'
+import { Starlogo } from './components/Starlogo'
 
 function App() {
+  console.log('App rendering')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PageTitle title={'INDEX'} />
+      <PageTitle title={'My friends'} />
+      <Header />
+      <Technologies />
+      <Raiting value={4} />
+      <Accordion title={'Friends list'} collapsed={false} />
+      <Accordion title={'Enemies list'} collapsed={false} />
+      <Starlogo />
+      <Starlogo />
+      <Starlogo />
+      <Starlogo />
+      <Starlogo />
+      <Raiting value={1} />
     </div>
-  );
+  )
 }
 
-export default App;
+type PageTitlePropsType = {
+  title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+  console.log('title rendering')
+  return <h1>{props.title}</h1>
+}
+
+export default App
